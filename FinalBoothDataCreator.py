89b061ccde29476352382a99b34a642838624ca6
Krishnendu1987 <46158456+Krishnendu1987@users.noreply.github.com>
2021-10-10 06:05:20 +0530

    Update FinalBoothDataCreator.py
    Added temp folder

--- FinalBoothDataCreator.py
+++ FinalBoothDataCreator.py
@@ -10,7 +10,8 @@ except ImportError:
     import Image
 
 path=r'C:\Users\91990\Documents\Anocracy\PurboMidnapore\NandiGram'
-temp1_path=r'C:\Users\91990\Documents\Anocracy\PurboMidnapore\NandiGram\temp2'
+temp1_path=r'C:\Users\91990\Documents\Anocracy\PurboMidnapore\NandiGram\temp1'
+temp2_path=r'C:\Users\91990\Documents\Anocracy\PurboMidnapore\NandiGram\temp2'
 for filename in os.listdir(path):
     pdf_file = os.path.join(path, filename)
     # checking if it is a file
@@ -36,3 +37,35 @@ for filename in os.listdir(path):
                 print(pages.index(page))
                 
             #print(pdf_file)
+for filename1 in os.listdir(temp1_path):
+    img_file1 = os.path.join(temp1_path, filename1)
+    if os.path.isfile(img_file1):
+        img_file1: str = img_file1.replace('\\', '/')
+        #mg_file_name1 = img_file1.split('/')[-1]
+        img_file_name1 = img_file1.split('/')[-1]
+        img = cv2.imread(img_file1)
+        original = img.copy()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # convert to grayscale
+        # threshold to get just the signature (INVERTED)
+        retval, thresh_gray = cv2.threshold(gray, thresh=100, maxval=255, \
+                                   type=cv2.THRESH_BINARY_INV)
+        cnts , hierarchy = cv2.findContours(thresh_gray,cv2.RETR_LIST, \
+                                       cv2.CHAIN_APPROX_SIMPLE)
+        
+        ROI_number = 0
+        min_area = 50000
+        max_area = 250000
+        for c in cnts:
+            x,y,w,h = cv2.boundingRect(c)
+            area = w*h
+            if area > min_area and area < max_area:
+                cv2.rectangle(img, (x, y), (x + w, y + h), (36,255,12), 2)
+                ROI = original[y:y+h, x:x+w]
+                img_file_name1 = img_file_name1[:-4]
+                #mg_file_name2 =str(img_name1) +"_img_cnt_" + {ROI_number} +".jpg"
+                img_file_name2 = str(img_file_name1) + "_img_cnt_" + str(ROI_number) +".jpg"
+                img_file2= os.path.join(temp2_path, img_file_name2)
+                img_file2: str = img_file2.replace('\\', '/')
+                #mg_name=f"C:/Users/91990/Documents/Anocracy/temp/Image_cont_{ROI_number}.jpg"
+                cv2.imwrite(img_file2.format(ROI_number), ROI)
+                ROI_number += 1
